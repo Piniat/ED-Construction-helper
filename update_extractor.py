cd35962b9a1d465eb8b71a17ebf315d439cdc56e
Piniat <98714215+Piniat@users.noreply.github.com>
2025-04-07 01:32:57 +0200

    Windows fixes

--- update_extractor.py
+++ update_extractor.py
@@ -2,8 +2,18 @@ import os
 from modules import state, extract_update
 import platform
 import time
+import ctypes
+import sys
+
+def run_as_admin():
+    if ctypes.windll.shell32.IsUserAnAdmin():
+        return True
+    else:
+        ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, ' '.join(sys.argv), None, 1)
+        os._exit(0)
 
 def extract():
+    run_as_admin()
     global platform
     platform = platform.system().lower()
     if platform == 'windows':
