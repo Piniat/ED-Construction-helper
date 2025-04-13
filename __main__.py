2de81865c8108f7ee5e5f72023aca1976b7acc6a
Piniat <mattkolo1109@gmail.com>
2025-04-13 21:32:06 +0200

    Updated logic for updater version check

--- __main__.py
+++ __main__.py
@@ -4,7 +4,7 @@ import sys
 import time
 import configparser
 import app
-from modules import state, updater, first_time_launch, clean_screen
+from modules import state, updater, first_time_launch, clean_screen, error_logger
 from prompt_toolkit import prompt
 
 def start_app():
@@ -28,16 +28,21 @@ def update_version_file():
     config.read("config.ini")
     auto_update = config["AUTO_UPDATE"]["value"]
     journal_folder = config['JOURNAL_PATH']['path']
+    updater_version = config['Updater_version']["version"]
+    firstlaunch = config["FIRST_TIME_LAUNCH"]["value"]
     config['JOURNAL_PATH'] = {'path': journal_folder}
     config['AUTO_UPDATE'] = {'value': auto_update}
     config['Version'] = {'version': CURRENT_VERSION}
+    config['FIRST_TIME_LAUNCH'] = {'value': firstlaunch}
+    config['Updater_version'] = {'version': updater_version}
+
     with open('config.ini', 'w') as configfile:
         config.write(configfile)
 
 def request_version():
     global CURRENT_VERSION
     try:
-        version_request = requests.get(state.REPO, timeout=4)
+        version_request = requests.get(state.REPO_ALL, timeout=4)
     except:
         print("Connection timeout. Skipping...")
         time.sleep(0.5)
@@ -46,31 +51,41 @@ def request_version():
     if api_request_status == 200:
         print("Api request successful")
         parsed_request = version_request.json()
-        state.last_release = parsed_request.get("tag_name")
-        if (state.last_release != CURRENT_VERSION) and (state.last_release != state.updater_verion):
-            if "updater "not in state.last_release:
+        state.last_release = parsed_request[0].get("tag_name")
+        #check for latest updater version
+        updater_releases = []
+        for release in parsed_request:
+            if "-updater" in release.get("tag_name"):
+                updater_releases.append(release.get("tag_name"))
+        state.last_updater_release = updater_releases[0]
+        if (state.last_release != CURRENT_VERSION) or (state.last_updater_release != state.updater_verion):
+            if state.last_release != CURRENT_VERSION:
             #print(f"A new update has released: {state.last_release}. Please go to https://github.com/Piniat/ED-Construction-helper/releases to download the latest release")
                 update_consent = prompt("Update found. Do you want to download it? y/n \n>")
                 if update_consent == "y":
                     updater.update()
                     print("Update complete. Please restart the app...")
                     time.sleep(1)
+                    #calling app.exit_app() with the () crashes for whatever reason
                     app.exit_app
                 elif update_consent == "n":
                     print("Skipping update...")
+                    time.sleep(0.5)
                     start_app()
                 else:
                     print("invalid option. starting app...")
                     start_app()
-            else:
+            elif state.last_updater_release != state.updater_verion:
                 update_consent = prompt("Updater update found. Do you want to download it? y/n \n>")
                 if update_consent == "y":
                     updater.updater_update()
                     print("Update complete. Please restart the app...")
                     time.sleep(1)
+                    #same as above...
                     app.exit_app
                 elif update_consent == "n":
                     print("Skipping update...")
+                    time.sleep(0.5)
                     start_app()
                 else:
                     print("invalid option. starting app...")
@@ -103,73 +118,82 @@ def request_version():
             print("Too many api errors. Skipping...")
             time.sleep(0.4)
             start_app()
-
-global CURRENT_VERSION
-global tries
-CURRENT_VERSION = "v0.7.0-rc3"
-state.current_version = CURRENT_VERSION
-global Stored_version
-missing_auto_update = False
-missing_version = False
-missing_journal = False
-missing_first_launch = False
-tries = 0
-if not os.path.isfile('config.ini'):
-    path = input("Input game journal file path without quotes:  \n")
+try:
+    global CURRENT_VERSION
+    global tries
+    CURRENT_VERSION = "v0.7.0-rc3"
+    state.current_version = CURRENT_VERSION
+    global Stored_version
+    missing_auto_update = False
+    missing_version = False
+    missing_journal = False
+    missing_first_launch = False
+    missing_updater_version = False
+    tries = 0
+    if not os.path.isfile('config.ini'):
+        path = input("Input game journal file path without quotes:  \n")
+        config = configparser.ConfigParser()
+        config['JOURNAL_PATH'] = {'path': path}
+        config['AUTO_UPDATE'] = {'value': True}
+        config['Version'] = {'version': CURRENT_VERSION}
+        config['FIRST_TIME_LAUNCH'] = {'value': True}
+        config['Updater_version'] = {'version': None}
+        with open('config.ini', 'w') as configfile:
+            config.write(configfile)
+        print("Config file created.")
+        time.sleep(1)
     config = configparser.ConfigParser()
-    config['JOURNAL_PATH'] = {'path': path}
-    config['AUTO_UPDATE'] = {'value': True}
-    config['Version'] = {'version': CURRENT_VERSION}
-    config['FIRST_TIME_LAUNCH'] = {'value': True}
-    with open('config.ini', 'w') as configfile:
-        config.write(configfile)
-    print("Config file created.")
-    time.sleep(1)
-config = configparser.ConfigParser()
-config.read('config.ini')
-config.sections()
-if not config.has_section("AUTO_UPDATE"):
-    missing_auto_update = True
-if not config.has_section("Version"):
-    missing_version = True
-if not config.has_section("JOURNAL_PATH"):
-    missing_journal = True
-if not config.has_section("FIRST_TIME_LAUNCH"):
-    missing_first_launch = True
-if missing_version:
-    config['Version'] = {'version': CURRENT_VERSION}
-if missing_journal:
-    path = input("Input game journal file path without quotes:  \n")
-    config['JOURNAL_PATH'] = {'path': path}
-if missing_auto_update:
-    config['AUTO_UPDATE'] = {'value': True}
-if missing_first_launch:
-    config['FIRST_TIME_LAUNCH'] = {'value': True}
-if missing_auto_update or missing_journal or missing_version or missing_first_launch:
-    with open('config.ini', 'w') as configfile:
-        config.write(configfile)
-        config.read('config.ini')
-        config.sections()
-Stored_version = config['Version']['version']
-if Stored_version != CURRENT_VERSION:
-    update_version_file()
-else:
-    CURRENT_VERSION = config['Version']['version']
-first_launch = config["FIRST_TIME_LAUNCH"]['value']
-if first_launch == "True":
-    clean_screen.clear_screen()
-    first_time_launch.onboarding()
-AUTO_UPDATE = config["AUTO_UPDATE"]["value"]
-if AUTO_UPDATE == "True":
-    if CURRENT_VERSION == None:
-        print("Error. Version number not found.")
-        request_version()
+    config.read('config.ini')
+    config.sections()
+    if not config.has_section("AUTO_UPDATE"):
+        missing_auto_update = True
+    if not config.has_section("Version"):
+        missing_version = True
+    if not config.has_section("JOURNAL_PATH"):
+        missing_journal = True
+    if not config.has_section("FIRST_TIME_LAUNCH"):
+        missing_first_launch = True
+    if not config.has_section("Updater_version"):
+        missing_updater_version = True
+    if missing_version:
+        config['Version'] = {'version': CURRENT_VERSION}
+    if missing_journal:
+        path = input("Input game journal file path without quotes:  \n")
+        config['JOURNAL_PATH'] = {'path': path}
+    if missing_auto_update:
+        config['AUTO_UPDATE'] = {'value': True}
+    if missing_first_launch:
+        config['FIRST_TIME_LAUNCH'] = {'value': True}
+    if missing_updater_version:
+        config['Updater_version'] = {'version': ""}
+    if missing_auto_update or missing_journal or missing_version or missing_first_launch or missing_updater_version:
+        with open('config.ini', 'w') as configfile:
+            config.write(configfile)
+            config.read('config.ini')
+            config.sections()
+    state.updater_verion = config["Updater_version"]["version"]
+    Stored_version = config['Version']['version']
+    if Stored_version != CURRENT_VERSION:
         update_version_file()
     else:
-        request_version()
-elif AUTO_UPDATE == "False":
-    print("Auto-update disabled. Skipping....")
-    time.sleep(0.4)
-    start_app()
-else:
-    print("Key error")
+        CURRENT_VERSION = config['Version']['version']
+    first_launch = config["FIRST_TIME_LAUNCH"]['value']
+    if first_launch == "True":
+        clean_screen.clear_screen()
+        first_time_launch.onboarding()
+    AUTO_UPDATE = config["AUTO_UPDATE"]["value"]
+    if AUTO_UPDATE == "True":
+        if CURRENT_VERSION == None:
+            print("Error. Version number not found.")
+            request_version()
+            update_version_file()
+        else:
+            request_version()
+    elif AUTO_UPDATE == "False":
+        print("Auto-update disabled. Skipping....")
+        time.sleep(0.4)
+        start_app()
+    else:
+        print("Key error")
+except:
+    error_logger.log_file_error()
